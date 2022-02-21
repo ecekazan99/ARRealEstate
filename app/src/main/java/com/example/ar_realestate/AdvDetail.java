5c9022225a70931e5d3279a366e6a0390efbad57
SefikaOzlem <sefika.pul@ceng.deu.edu.tr>
2022-02-21 22:58:32 +0300

    Detail

---
+++ app/src/main/java/com/example/ar_realestate/AdvDetail.java
@@ -0,0 +1,145 @@
+package com.example.ar_realestate;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.ArrayList;
+
+public class AdvDetail {
+    private String advTitle, advStatus, roomNum, warmType, elgForCredit, usingStatus, buildType, itemStatus, stateBuilding, swap, front, fuelType, date, address;
+    private int price, squareMeters, buildingFloors, floorLoc, buildAge, numOfBathr, rentalIncome, dues;
+
+    private long latitude, longitude;
+    private Bitmap adv_image;
+
+
+    public AdvDetail(String advTitle,Bitmap adv_image, int price, String advStatus, String roomNum, int squareMeters, int buildingFloors, int floorLoc, int buildAge, String buildType, String itemStatus, String warmType, int numOfBathr, String elgForCredit, String usingStatus, String stateOfBuilding, int rentalIncome, int dues, String swap, String front, String fuelType, String date, String address) {
+        this.advTitle = advTitle;
+        this.adv_image=adv_image;
+        this.price = price;
+        this.advStatus = advStatus;
+        this.roomNum = roomNum;
+        this.squareMeters = squareMeters;
+        this.buildingFloors = buildingFloors;
+        this.floorLoc = floorLoc;
+        this.buildAge = buildAge;
+        this.buildType = buildType;
+        this.itemStatus = itemStatus;
+        this.warmType = warmType;
+        this.numOfBathr = numOfBathr;
+        this.elgForCredit = elgForCredit;
+        this.usingStatus = usingStatus;
+        this.stateBuilding = stateOfBuilding;
+        this.rentalIncome = rentalIncome;
+        this.dues = dues;
+        this.swap = swap;
+        this.front = front;
+        this.fuelType = fuelType;
+        this.date = date;
+        this.address = address;
+    }
+
+    public Bitmap getAdv_image() {
+        return adv_image;
+    }
+
+    public String getAdvTitle() {
+        return advTitle;
+    }
+
+    public String getAdvStatus() {
+        return advStatus;
+    }
+
+    public String getWarmType() {
+        return warmType;
+    }
+
+    public String getElgForCredit() {
+        return elgForCredit;
+    }
+
+    public String getUsingStatus() {
+        return usingStatus;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getRoomNum() {
+        return roomNum;
+    }
+
+    public int getSquareMeters() {
+        return squareMeters;
+    }
+
+    public int getFloorLoc() {
+        return floorLoc;
+    }
+
+    public int getBuildAge() {
+        return buildAge;
+    }
+
+    public int getNumOfBathr() {
+        return numOfBathr;
+    }
+
+    public int getRentalIncome() {
+        return rentalIncome;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public String getBuildType() {
+        return buildType;
+    }
+
+    public String getItemStatus() {
+        return itemStatus;
+    }
+
+    public String getSwap() {
+        return swap;
+    }
+
+    public String getFront() {
+        return front;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getBuildingFloors() {
+        return buildingFloors;
+    }
+
+    public int getDues() {
+        return dues;
+    }
+
+    public String getStateBuilding() {
+        return stateBuilding;
+    }
+
+}
--- app/src/main/java/com/example/ar_realestate/AdvDetailActivity.java
+++ app/src/main/java/com/example/ar_realestate/AdvDetailActivity.java
@@ -1,14 +1,112 @@
 package com.example.ar_realestate;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ar_realestate.R;
+
 
 public class AdvDetailActivity extends AppCompatActivity {
+
+
+    private ImageView advImageView;
+    private TextView txtAdvTitle, txtAdvPrice, txtAdvStatus, txtAdvRoomNum, txtSquareMeter,
+            txtBuildingFloors, txtFloorLocation, txtBuildingAge, txtBuildingType, txtItemStatus, txtWarmType, txtNumOfBathr,
+            txtElgForCredit, txtUsingStatus, txtStateBuilding, txtRentalIncome, txtDues, txtSwap, txtFront,
+            txtFuelType, txtDate, txtAddress;
+
+    String advTitle, advStatus, roomNum, warmType, elgForCredit, usingStatus, buildType, itemStatus, stateBuilding, swap, front, fuelType, date, address;
+    int price, squareMeters, buildingFloors, floorLoc, buildAge, numOfBathr, rentalIncome, dues;
+    long latitude, longitude;
+
+    private Bitmap advImagePng;
+
+    private void init() {
+        advImageView = (ImageView) findViewById(R.id.detail_AdvImage);
+        txtAdvTitle = (TextView) findViewById(R.id.detail_AdvTitle);
+        txtAdvPrice = (TextView) findViewById(R.id.detail_AdvPrice);
+        txtAdvStatus = (TextView) findViewById(R.id.detail_AdvStatus);
+        txtAdvRoomNum = (TextView) findViewById(R.id.detail_AdvRoomNum);
+        txtSquareMeter = (TextView) findViewById(R.id.detail_AdvSquareMeter);
+        txtBuildingFloors = (TextView) findViewById(R.id.add_adv_TextBuildingFloors);
+        txtFloorLocation = (TextView) findViewById(R.id.detail_AdvFloorLoc);
+        txtBuildingAge = (TextView) findViewById(R.id.detail_AdvBuildingAge);
+        txtBuildingType = (TextView) findViewById(R.id.add_adv_TextBuildType);
+        txtItemStatus = (TextView) findViewById(R.id.detail_AdvItemStatus);
+        txtWarmType = (TextView) findViewById(R.id.detail_AdvWarmType);
+        txtNumOfBathr = (TextView) findViewById(R.id.detail_AdvNumOfBathr);
+        txtElgForCredit = (TextView) findViewById(R.id.detail_AdvElgCredit);
+        txtUsingStatus = (TextView) findViewById(R.id.detail_AdvUsingStatus);
+        txtStateBuilding = (TextView) findViewById(R.id.detail_AdvStateOfBuilding);
+        txtRentalIncome = (TextView) findViewById(R.id.detail_AdvRentalIncome);
+        txtDues = (TextView) findViewById(R.id.detail_AdvDues);
+        txtSwap = (TextView) findViewById(R.id.detail_AdvSwap);
+        txtFront = (TextView) findViewById(R.id.detail_AdvFront);
+        txtFuelType = (TextView) findViewById(R.id.detail_AdvFuelType);
+        txtDate = (TextView) findViewById(R.id.detail_AdvDate);
+        txtAddress = (TextView) findViewById(R.id.detail_AdvAddress);
+
+        // advTitle= HomeFragment.advDetail.getAdvTitle();
+      /*  advTitle=HomeFragment.advDetail.getAdvTitle();
+        advImagePng=HomeFragment.advDetail.getAdv_image();
+        price=HomeFragment.advDetail.getPrice();
+        advStatus=HomeFragment.advDetail.getAdvStatus();
+        roomNum=HomeFragment.advDetail.getRoomNum();
+        squareMeters=HomeFragment.advDetail.getSquareMeters();
+        buildingFloors=HomeFragment.advDetail.getBuildingFloors();
+        floorLoc=HomeFragment.advDetail.getFloorLoc();
+        buildAge=HomeFragment.advDetail.getBuildAge();
+        buildType=HomeFragment.advDetail.getBuildType();
+        itemStatus=HomeFragment.advDetail.getItemStatus();
+        warmType=HomeFragment.advDetail.getWarmType();
+        numOfBathr=HomeFragment.advDetail.getNumOfBathr();
+        elgForCredit=HomeFragment.advDetail.getElgForCredit();
+        usingStatus=HomeFragment.advDetail.getUsingStatus();
+        stateBuilding=HomeFragment.advDetail.getStateBuilding();
+        rentalIncome=HomeFragment.advDetail.getRentalIncome();
+        dues=HomeFragment.advDetail.getDues();
+        swap=HomeFragment.advDetail.getSwap();
+        front=HomeFragment.advDetail.getFront();
+        fuelType=HomeFragment.advDetail.getFuelType();
+        date=HomeFragment.advDetail.getDate();
+        address=HomeFragment.advDetail.getAddress();*/
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adv_detail);
+        init();
+
+        txtAdvTitle.setText(advTitle);
+        advImageView.setImageBitmap(advImagePng);
+        txtAdvPrice.setText(price);
+        txtAdvStatus.setText(advStatus);
+        txtAdvRoomNum.setText(roomNum);
+        txtSquareMeter.setText(squareMeters);
+        txtBuildingFloors.setText(buildingFloors);
+        txtFloorLocation.setText(floorLoc);
+        txtBuildingAge.setText(buildAge);
+        txtBuildingType.setText(buildType);
+        txtItemStatus.setText(itemStatus);
+        txtWarmType.setText(warmType);
+        txtNumOfBathr.setText(numOfBathr);
+        txtElgForCredit.setText(elgForCredit);
+        txtUsingStatus.setText(usingStatus);
+        txtStateBuilding.setText(stateBuilding);
+        txtRentalIncome.setText(rentalIncome);
+        txtDues.setText(dues);
+        txtSwap.setText(swap);
+        txtFront.setText(front);
+        txtFuelType.setText(fuelType);
+        txtDate.setText(date);
+        txtAddress.setText(address);
+
+
     }
 }
