5db3388cbd7da596d672ef8d43cc9eb733ddafb0
SefikaOzlem <sefika.pul@ceng.deu.edu.tr>
2022-03-04 19:42:52 +0300

    Myadv Viewing(not yet completed detail in this part)

--- app/src/main/java/com/example/ar_realestate/Advertisement.java
+++ app/src/main/java/com/example/ar_realestate/Advertisement.java
@@ -224,6 +224,7 @@ public class Advertisement {
 
     static public ArrayList<Advertisement> getData(Context context){
 
+        int advertisementId=0;
         String sqlQuery="";
         Cursor cursor = null;
         ArrayList<Advertisement> advertisementList=new ArrayList<>();
@@ -253,7 +254,7 @@ public class Advertisement {
         ArrayList<Integer> numOfBathrList=new ArrayList<>();
         ArrayList<Integer> rentalIncomeList=new ArrayList<>();
         ArrayList<Integer> duesList=new ArrayList<>();
-
+        ArrayList<Integer> advIdList=new ArrayList<>();
         ArrayList<Bitmap> ImageList=new ArrayList<>();
 
         ArrayList<Long>xCoordinateList=new ArrayList<>();
@@ -279,7 +280,7 @@ public class Advertisement {
 
 
             }
-            else if(FilterAdvFragment.applButton!=true && MainActivity.incrPriceClick==false && MainActivity.decrsPriceClick==false)
+            else if(FilterAdvFragment.applButton!=true && MainActivity.incrPriceClick==false && MainActivity.decrsPriceClick==false && MyAccountFragment.clickMyAdv!=true)
             {
                 System.out.println("Burayaa girdii 2222222222");
                 sqlQuery="SELECT * FROM Advertisements";
@@ -296,6 +297,20 @@ public class Advertisement {
                 System.out.println("Burayaa girdii 4444444444");
                 sqlQuery="SELECT * FROM Advertisements ORDER BY Price DESC";
                 cursor=MainActivity.db.rawQuery(sqlQuery,null);
+            }
+            else if(MyAccountFragment.clickMyAdv==true)
+            {
+                System.out.println("Burayaa girdii 555555");
+                sqlQuery="SELECT AdvId FROM  UserAdvertisement WHERE UserId = ? ";
+                cursor=MainActivity.db.rawQuery(sqlQuery, new String[] { String.valueOf(MyAccountFragment.userMyId)});
+                while (cursor.moveToNext()){
+                    System.out.println("ADVVVVVVVV IDDDDDDD: "+cursor.getString(0));
+                    advertisementId= Integer.parseInt(cursor.getString(0));
+                    cursor.close();
+                }
+                sqlQuery="SELECT * FROM  Advertisements WHERE AdvId = ? ";
+                cursor=MainActivity.db.rawQuery(sqlQuery, new String[] { String.valueOf(advertisementId)});
+
             }
 
 
@@ -326,6 +341,7 @@ public class Advertisement {
             int townIndex=cursor.getColumnIndex("Town");
             int xCoordinateIndex=cursor.getColumnIndex("xCoordinate");
             int yCoordinateIndex=cursor.getColumnIndex("yCoordinate");
+            int advIdIndex=cursor.getColumnIndex("AdvId");
 
             while (cursor.moveToNext()){
                 advTitleList.add(cursor.getString(advTitleIndex));
@@ -353,7 +369,7 @@ public class Advertisement {
                 numOfBathrList.add(cursor.getInt(numOfBathroomsIndex));
                 rentalIncomeList.add(cursor.getInt(rentalIncomeIndex));
                 duesList.add(cursor.getInt(duesIndex));
-
+                advIdList.add(cursor.getInt(advIdIndex));
                 byte[] imageByte=cursor.getBlob(ImageIndex);
                 Bitmap imageAdv= BitmapFactory.decodeByteArray(imageByte,0,imageByte.length);
                 ImageList.add(imageAdv);
@@ -392,18 +408,18 @@ public class Advertisement {
                 adv.setTown(townList.get(i));
                 adv.setLatitude(xCoordinateList.get(i));
                 adv.setLongitude(yCoordinateList.get(i));
-
-
-
+                adv.setAdvId(advIdList.get(i));
                 advertisementList.add(adv);
 
             }
         }catch (Exception e){
             e.printStackTrace();
         }
+        MyAccountFragment.clickMyAdv=false;
         MainActivity.decrsPriceClick=false;
         MainActivity.incrPriceClick=false;
         FilterAdvFragment.applButton=false;
         return advertisementList;
     }
+
 }
--- app/src/main/java/com/example/ar_realestate/MyAdvertisementFragment.java
+++ app/src/main/java/com/example/ar_realestate/MyAdvertisementFragment.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MyAdvertisementFragment extends Fragment {
+    private AdvertisementAdapter advAdapter;
+    static public AdvDetail advDetail;
+    RecyclerView recyclerView;
+    ArrayList<Advertisement> adv;
+
 
 
     public MyAdvertisementFragment() {
@@ -33,6 +44,40 @@ public class MyAdvertisementFragment extends Fragment {
 
         System.out.println(user.getUserId());
 
-        return inflater.inflate(R.layout.fragment_my_advertisement, container, false);
+
+        View view=inflater.inflate(R.layout.fragment_my_advertisement,container,false);
+        recyclerView=(RecyclerView) view.findViewById(R.id.recviewMyAdvs);
+        // burayı ekledim
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        advAdapter=new AdvertisementAdapter(Advertisement.getData(getContext()),getContext());
+
+        recyclerView.setAdapter(advAdapter);
+        //burayı ekledim
+
+        advAdapter.setOnItemClickListener(adv ->  {
+
+            advDetail=new AdvDetail(adv.getAdvTitle(),adv.getAdvImage(),adv.getPrice(),adv.getAdvStatus(),
+                    adv.getRoomNum(),adv.getSquareMeters(),adv.getBuildingFloors(),adv.getFloorLoc(),
+                    adv.getBuildAge(),adv.getBuildType(),adv.getItemStatus(),adv.getWarmType(),adv.getNumOfBathr(),
+                    adv.getElgForCredit(),adv.getUsingStatus(),adv.getStateBuilding(),adv.getRentalIncome(),
+                    adv.getDues(),adv.getSwap(),adv.getFront(),adv.getFuelType(),adv.getDate(),adv.getAddress(),adv.getCity(),adv.getTown());
+
+            MainActivity.navViewToolbar.setVisibility(View.INVISIBLE);
+            MainActivity.navViewToolbar_detail.setVisibility(View.VISIBLE);
+            replaceFragment(new AdvDetailFragment());
+            // MainActivity.navViewToolbar_detail.setVisibility(View.INVISIBLE);
+        }); // buraya kadar kısım
+
+        return view;
     }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager=getFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment_activity_main,fragment);
+        fragmentTransaction.commit();
+    }
+
 }
